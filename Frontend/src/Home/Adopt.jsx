73fa0a759b620a062f97c8c64c24402dd846c6b8
NehaK-XP <nehakamath.241cs240@nitk.edu.in>
2025-09-28 00:07:55 +0530

    updated adoption page sidebar...

--- Frontend/src/Home/Adopt.jsx
+++ Frontend/src/Home/Adopt.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
 
 const Adopt = () => {
-  return (
-    <div>Adopt</div>
-  )
+    const navigate = useNavigate
+
+    return (
+        <>
+        <div className="w-64 h-screen bg-amber-800 text-white flex flex-col p-4">
+            <h2 className="text-xl font-bold mb-6">Dashboard</h2>
+            <button onClick={() => navigate("/adopt")} className="mb-4 text-left hover:text-black hover:cursor-pointer">
+                All Listings
+            </button>
+            <button  className="mb-4 text-left hover:text-black hover:cursor-pointer">
+                Your Listings
+            </button>
+            <button onClick={() => navigate("/queries")} className="mb-4 text-left hover:text-black hover:cursor-pointer">
+                Queries
+            </button>
+        </div>
+        </>
+    )
 }
 
 export default Adopt
